476922efbfa4c5c63c93f40f20e895c5dbe7b870
Joylean Anna Kingston <joyleanannakingston10@gmail.com>
2023-06-14 20:49:49 +0400

    form completed

--- src/app/admin/home/home.component.ts
+++ src/app/admin/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-
+import { FormGroup, FormControl, FormArray, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import timeslot from './time_slot.json';
+import cartype from './car_type.json';
 
 @Component({
   selector: 'app-home',
@@ -9,45 +10,66 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
-  adminForm = new FormGroup({
-    Name: new FormControl(''),
-    date: new FormControl(''),
-  });
+  adminForm!: FormGroup;
 
   //multiselect dropdown
   dropdownList: any[] = [];
   selectedItems: any[] = [];
   dropdownSettings: any = {};
+  dropdownListCars: any[] = [];
+  selectedCars: any[] = [];
+  dropdownSettingsCar: any = {};
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    // this.dropdownList = [
-    //   { item_id: 1, item_text: 'Mumbai' },
-    //   { item_id: 2, item_text: 'Bangaluru' },
-    //   { item_id: 3, item_text: 'Pune' },
-    //   { item_id: 4, item_text: 'Navsari' },
-    //   { item_id: 5, item_text: 'New Delhi' }
-    // ];
-    // this.selectedItems = [
-    //   { item_id: 3, item_text: 'Pune' },
-    //   { item_id: 4, item_text: 'Navsari' }
-    // ];
-    // this.dropdownSettings = {
-    //   singleSelection: false,
-    //   idField: 'item_id',
-    //   textField: 'item_text',
-    //   selectAllText: 'Select All',
-    //   unSelectAllText: 'UnSelect All',
-    //   // itemsShowLimit: 3,
-    //   allowSearchFilter: true
-    // };
+    this.initForm();
+    console.log(timeslot, cartype);
+    this.dropdownList = timeslot;
+    this.dropdownListCars = cartype;
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      // itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+    this.dropdownSettingsCar = {
+      singleSelection: false,
+      idField: 'car_id',
+      textField: 'car_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      // itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+  initForm(){
+    this.adminForm= this.fb.group ({
+      name: ['',[Validators.required, Validators.minLength(2)]],
+      date: [new Date(),[Validators.required,this.dateValidator]],
+      time_slots_available:['',[Validators.required]],
+      car_type:['',[Validators.required]],
+      price:['',[Validators.required,Validators.pattern("^[1-9][0-9]*$")]],
+      number_of_slots:['',[Validators.required,Validators.pattern("^[1-9][0-9]*$")]],
+      cancel:['',[Validators.required,Validators.pattern("^[1-9][0-9]*$")]]
+    });
   }
 
   onItemSelect(item: any) {
     console.log(item);
   }
   onSelectAll(items: any) {
+    console.log(items);
+  }
+
+  onItemSelectCar(item: any) {
+    console.log(item);
+  }
+  onSelectAllCar(items: any) {
     console.log(items);
   }
 
@@ -64,4 +86,30 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  dateValidator(control: AbstractControl){ 
+    if(control){
+      // const selected = new Date(control.value); changing it into detailed date
+      const selected=control.value;
+      const todayComplex = new Date();
+
+    let month = '' + (todayComplex.getMonth() + 1);
+    let day = '' + todayComplex.getDate();
+    const year = todayComplex.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    const today=[year, month, day].join('-');
+  
+      console.log(selected, today);
+      if(!(control && selected)) {
+        // if there's no control or no value, that's ok
+        return null;
+      }
+  
+      // return null if there's no errors
+      return (selected > today || selected === today)
+        ? null
+        : {invalidDate: 'You cannot use past dates' } ;
+    }
+    return null;
+  }
 }
